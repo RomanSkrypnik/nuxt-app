9118e824c63761a74ce214a794b3ebe67ef2cdb8
unknown <rorchenko@gmail.com>
2022-10-18 17:09:26 +0300

    login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/supabase'],
     css: ['~/assets/styles/main.scss'],
     build: {
         postcss: {
