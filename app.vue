15f18e0cd7fcbf8842949da6aac7d868214b3666
unknown <rorchenko@gmail.com>
2022-10-18 16:37:47 +0300

    defu fix

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </div>
 </template>
+<script>
+import '@/assets/styles/main.scss'
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    css: ['~/assets/styles/main.scss'],
+    build: {
+        postcss: {
+            postcssOptions: require('./postcss.config.js'),
+        },
+    }
 })
