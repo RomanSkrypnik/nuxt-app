163e8ee9b8d1ca73a052e5983a60a28619630988
unknown <rorchenko@gmail.com>
2022-10-20 00:29:52 +0300

    multiple

--- components/inputs/FileUpload.vue
+++ components/inputs/FileUpload.vue
@@ -3,6 +3,7 @@
         <div class='mb-3 w-96'>
             <label class='form-label inline-block mb-2 text-gray-700'>{{ placeholder }}</label>
             <input
+                :multiple='multiple'
                 @change='handleChange'
                 class='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                 type='file'
@@ -14,15 +15,16 @@
 interface Props {
     placeholder?: string;
     className?: string;
+    multiple?: boolean;
 }
 
-const { placeholder, className } = withDefaults(defineProps<Props>(), {
+const { placeholder, multiple, className } = withDefaults(defineProps<Props>(), {
     placeholder: 'Upload file',
+    multiple: false,
 });
 
-const file = ref<null | File>(null);
-
-const handleChange = async () => {
-    console.log('selected file', file?.value);
+const handleChange = async (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    console.log('selected file', target.files);
 };
 </script>
