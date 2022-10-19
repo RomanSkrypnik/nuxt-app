25f619ac8eb71b15430f16032589d0470afa84ba
unknown <rorchenko@gmail.com>
2022-10-19 20:50:00 +0300

    with provided menu dropdown

---
+++ components/ui/Avatar.vue
@@ -0,0 +1,45 @@
+<template>
+    <Menu as='div' class='relative inline-block text-left'>
+        <MenuButton
+            class='inline-flex w-full justify-center bg-white text-sm font-medium text-gray-700'>
+            <div class='block lg:flex lg:items-center lg:w-auto'>
+                <img class='inline-block h-10 w-10 rounded-full ring-2 ring-white'
+                     src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                     alt='Avatar'
+                >
+            </div>
+        </MenuButton>
+        <transition
+            v-if='props.items'
+            enter-active-class='transition ease-out duration-100'
+            enter-from-class='transform opacity-0 scale-95'
+            enter-to-class='transform opacity-100 scale-100'
+            leave-active-class='transition ease-in duration-75'
+            leave-from-class='transform opacity-100 scale-100'
+            leave-to-class='transform opacity-0 scale-95'>
+            <MenuItems
+                class='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div class='py-1'>
+                    <MenuItem v-slot='{ active }' v-for='item in props.items' @click='item.click'>
+                        <a href='#'
+                           :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
+                            {{ item.label }}
+                        </a>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
+</template>
+
+<script setup lang='ts'>
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+
+interface Props {
+    click?: () => void;
+    src?: string;
+    items?: { label: String, click: Function }[];
+}
+
+const props = defineProps<Props>();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,14 @@
     <Card>
         <div class='flex flex-col items-center'>
             <h2 class='font-medium leading-tight text-1xl mt-0 mb-2'>Welcome {{ user?.email }} !</h2>
-            <Button class-name='mt-2' @click='signOut'>Sign out</Button>
         </div>
     </Card>
 </template>
 
 <script setup lang='ts'>
 import Card from '../components/Card';
-import Button from '../components/Button';
-
-import { FetchError } from 'ohmyfetch';
 
 definePageMeta({ middleware: 'auth', layout: 'authorized' });
 
-const supabase = useSupabaseClient();
 const user = useSupabaseUser();
-
-async function signOut() {
-    try {
-        await supabase.auth.signOut();
-    } catch (error) {
-        if (error instanceof FetchError) {
-            alert(error.message);
-        }
-    }
-}
 </script>
