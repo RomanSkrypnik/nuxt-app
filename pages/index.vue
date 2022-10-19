9f9db08d3dbfb9fb5c090099b5cb4f34b5687cdb
unknown <rorchenko@gmail.com>
2022-10-19 21:52:38 +0300

    without react-query

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <template>
     <Card>
-        <div class='flex flex-col items-center'>
-            <h2 class='font-medium leading-tight text-1xl mt-0 mb-2'>Welcome {{ user?.email }} !</h2>
+        <div class='flex items-center'>
+            <h2 class='font-medium leading-tight text-1xl mt-0 mb-2'>{{ user?.email }}</h2>
         </div>
     </Card>
 </template>
 
 <script setup lang='ts'>
 import Card from '../components/Card';
+import img from '@/assets/images/avatar.png';
 
 definePageMeta({ middleware: 'auth', layout: 'authorized' });
 
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const { data } = await supabase
+    .from('profiles')
+    .select('username, avatar_url')
+    .eq('id', user.value?.id)
+    .single();
 </script>
